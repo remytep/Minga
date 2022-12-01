56b5c361fb3af6290dbb943233829f4a7d18c014
gaelWumba <gael.wumba-buatu@epitech.eu>
2022-12-01 17:45:38 +0100

    Front

--- minga-frontend/src/App.js
+++ minga-frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 // Pages
 import Accueil from './pages/Accueil';
-import ProductDetail from './pages/ProductDetail';
+import DetailedProduct from './components/products/DetailedProduct';
 
 // Components
 import Header from './components/homePage/Header'
@@ -12,17 +13,20 @@ import Footer from './components/homePage/Footer'
 
 function App() {
   return (
-    <div>
+    
       <Router>
+        <div>
         <Header />
         <Routes>
           <Route path='/' element={<Accueil />} />
-          <Route path='/product/:id' element={<ProductDetail />} />
+          <Route path='/products/:id' element={<DetailedProduct />} />
+          <Route  path='/login' element={<Login />} />
+          <Route  path='/register' element={<Register />} />
         </Routes>
         <Footer />
         <ShoppingBag />
+        </div>
       </Router>
-    </div>
   );
 }
 
