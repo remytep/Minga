6fd86144580698cabb949c9ea74ae8bd89003ee3
prism <veronique.vdang@gmail.com>
2022-12-02 16:59:10 +0100

    pulling the updated version with admin

--- minga-frontend/src/components/homePage/Product.js
+++ minga-frontend/src/components/homePage/Product.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useContext } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { BsPlus, BsEyeFill } from 'react-icons/bs'
 
