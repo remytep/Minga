60d215eac79f8ade8afef4f7c6ce2971ef62ba50
Ruben <rubenmuya9129@gmail.com>
2022-12-06 10:33:15 +0100

    fix getProducts axios error

--- minga-frontend/src/contexts/ProductContext.jsx
+++ minga-frontend/src/contexts/ProductContext.jsx
@@ -8,15 +8,15 @@ function ProductProvider({ children }) {
 
 
     useEffect(() => {
-        const getProducts = async () => {
-            const response = await axios({
-                method: 'GET',
-                url: "https://fakestoreapi.com/products",
-                headers: { "content-type": "application/json" },
-            });
-            setProducts(response.data);
-        };
-        getProducts();
+        // const getProducts = async () => {
+        //     const response = await axios({
+        //         method: 'GET',
+        //         url: "https://fakestoreapi.com/products",
+        //         headers: { "content-type": "application/json" },
+        //     });
+        //     setProducts(response.data);
+        // };
+        // getProducts();
     }, [])
     return (
         <ProductContext.Provider value={{ products }}>{children}</ProductContext.Provider>
