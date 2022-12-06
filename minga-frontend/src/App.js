97834e666a95a2db015120d3cb5b2d8ae3155fc0
Ruben <rubenmuya9129@gmail.com>
2022-12-06 10:31:01 +0100

    addadd route admin

--- minga-frontend/src/App.js
+++ minga-frontend/src/App.js
@@ -11,21 +11,23 @@ import Login from './components/users/Login';
 import Register from './components/users/Register';
 import DetailedProduct from './components/detailedProduct/DetailedProduct'
 import Filter from './components/homePage/Filter';
+import Admin from './components/ApiPlatform';
 
 function App() {
   return (
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Accueil />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/products/:id' element={<DetailedProduct />} />
-        </Routes>
-        <Footer />
-        <ShoppingBag />
-        <Filter/>
-      </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Accueil />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/products/:id' element={<DetailedProduct />} />
+        <Route path='/admin/*' element={<Admin />} />
+      </Routes>
+      <Footer />
+      <ShoppingBag />
+      <Filter />
+    </Router>
   );
 }
 
